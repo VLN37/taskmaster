d9d95f445ac9b50c30d534762705f4c796ba4739
VLN37 <jfpr1237@gmail.com>
2023-11-09 19:35:15 -0300

    tests: execve example

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,4 +1,11 @@
+use std::process;
+use std::io::Write;
+use masterlib::config;
 
 fn main() {
-    println!("Hello, world!");
+    let file = std::fs::File::open("config.yml").expect("Could not open file.");
+    let config = config::read(file);
+    let program = &config.programs["echo"];
+    let output = process::Command::new(&program.command).arg("This compiles!").output();
+    std::io::stdout().write_all(&output.unwrap().stdout).unwrap();
 }
