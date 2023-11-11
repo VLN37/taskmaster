c5b0886acce631ba36ef4188eac72efc6c17b289
VLN37 <jfpr1237@gmail.com>
2023-11-11 13:15:57 -0300

    tests: ensure config is valid

--- masterlib/src/bin/main.rs
+++ masterlib/src/bin/main.rs
@@ -1,6 +1,9 @@
-use masterlib::daemon::config::Program;
+use masterlib::daemon::{config::Program, TaskMasterConfig};
+use std::fs;
 
 fn main() {
     let var = Program::new();
+    println!("{:?}", var);
+    let var = TaskMasterConfig::from(fs::File::open(masterlib::CONFIG_PATH).unwrap());
     println!("{:?}", var);
 }
--- masterlib/src/daemon/config.rs
+++ masterlib/src/daemon/config.rs
@@ -1,8 +1,8 @@
-pub mod exceptions;
-pub mod structs;
-
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, fs::File};
+
+pub mod exceptions;
+pub mod structs;
 pub use structs::{Program, RestartOption, Signal};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -12,4 +12,42 @@ pub struct TaskMasterConfig {
 
 pub fn read(file: File) -> Result<TaskMasterConfig, serde_yaml::Error> {
     serde_yaml::from_reader(file)
+}
+
+impl From<File> for TaskMasterConfig {
+    fn from(file: File) -> Self {
+        match serde_yaml::from_reader(file) {
+            Ok(config) => config,
+            Err(e) => panic!("{:?}", e)
+        }
+    }
+}
+
+
+#[macro_export]
+macro_rules! assert_ok {
+    ( $x:expr ) => {
+        match $x {
+            std::result::Result::Ok(v) => v,
+            std::result::Result::Err(e) => {
+                panic!("Error calling {}: {:?}", stringify!($x), e);
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn config_parse_test() {
+        let manifest = String::from(env!("CARGO_MANIFEST_DIR"));
+        let remove = manifest.find("/masterlib").unwrap();
+        let mut root = String::from(&manifest[..remove]);
+        root.push_str(crate::CONFIG_PATH);
+        let f = std::fs::File::open(root).expect("Could not open file.");
+        let config = self::read(f);
+        assert_ok!(config);
+    }
 }
