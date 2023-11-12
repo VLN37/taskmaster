85fdcfa532ac5a8c3cd29b875f46d8c88745ccaf
VLN37 <jfpr1237@gmail.com>
2023-11-12 00:26:55 -0300

    feat: blocking version of the loop

--- ctl/src/main.rs
+++ ctl/src/main.rs
@@ -1,3 +1,28 @@
+use std::{
+    io::{Read, Write},
+    net::Shutdown,
+    os::unix::net::UnixStream,
+};
+
 fn main() {
-    println!("this compiles!")
+    let mut buf = String::new();
+    buf.reserve(500);
+    println!("Ready for input");
+    loop {
+        let mut stream = UnixStream::connect(masterlib::SOCKET_PATH).unwrap();
+        match std::io::stdin().read_line(&mut buf) {
+            Ok(_) => (),
+            Err(e) => println!("stdin: {e}"),
+        };
+        buf.pop();
+        stream
+            .write_all(buf.as_bytes())
+            .unwrap_or_else(|e| println!("client error: {e:?}"));
+        buf.clear();
+        stream.shutdown(Shutdown::Write).unwrap();
+        stream.read_to_string(&mut buf).unwrap();
+        stream.shutdown(Shutdown::Read).unwrap();
+        println!("response: {buf}");
+        buf.clear();
+    }
 }
--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,3 +1,18 @@
+use masterlib::daemon::BackEnd;
+
 fn main() {
-    println!("this compiles!");
+    let backend = BackEnd::new();
+    println!("Awaiting front-end connection");
+    backend.accept();
+    for conn in backend.socket.incoming() {
+        println!("CONNECTED");
+        let mut client = match conn {
+            Ok(c) => c,
+            Err(e) => {
+                println!("connect failed {e:?}");
+                continue;
+            }
+        };
+        backend.process(&mut client).unwrap_or_else(|x| println!("err: {x:?}"));
+    }
 }
