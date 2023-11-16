2d16150a1b3945c4b8d786601e24e6471015303a
Paulo Santana <paulo.santana.r@gmail.com>
2023-11-16 18:26:52 -0300

    feat: add sigaction interface

--- daemon/examples/server.rs
+++ daemon/examples/server.rs
@@ -1,6 +1,23 @@
 use std::io::Read;
+use std::mem::MaybeUninit;
 use std::os::unix::net::UnixListener;
 use std::path::Path;
+
+// use libc::{sigaction, sigemptyset, siginfo_t, SA_SIGINFO};
+
+use masterlib::daemon::config::structs::{
+    sigaction, sigemptyset, siginfo_t, SA_SIGINFO,
+};
+use masterlib::daemon::config::Signal;
+
+// use crate::mini_sigaction::{sigaction, sigemptyset, siginfo_t, SA_SIGINFO};
+
+fn handler(sig: i32, info: siginfo_t) {
+    let pid = unsafe { info.si_pid() };
+    panic!("signal caught: {sig}, pid: {:?}", pid);
+}
+
+mod mini_sigaction {}
 
 // cargo run -p daemon --example server
 fn main() {
@@ -14,6 +31,16 @@ fn main() {
         Ok(stream) => stream,
     };
     println!("server started");
+    println!("pid: {}", std::process::id());
+
+    let mut action: sigaction = unsafe { MaybeUninit::zeroed().assume_init() };
+    action.sa_sigaction = handler as usize;
+    action.sa_flags = SA_SIGINFO;
+    unsafe { sigemptyset(&mut action.sa_mask) };
+    let mut old_action: sigaction = unsafe { MaybeUninit::zeroed().assume_init() };
+
+    unsafe { sigaction(2, &action, &mut old_action) };
+    unsafe { sigaction(Signal::SIGHUP as i32, &action, &mut old_action) };
 
     for client in listener.incoming() {
         let mut buffer = String::new();
--- masterlib/src/daemon/config/structs/signals.rs
+++ masterlib/src/daemon/config/structs/signals.rs
@@ -55,3 +55,127 @@ pub enum Signal {
     /// Virtual time alarm
     SIGVTALRM,
 }
+
+use std::ffi::{c_int, c_long};
+
+// N.B., for LLVM to recognize the void pointer type and by extension
+//     functions like malloc(), we need to have it represented as i8* in
+//     LLVM bitcode. The enum used here ensures this and prevents misuse
+//     of the "raw" type by only having private variants. We need two
+//     variants, because the compiler complains about the repr attribute
+//     otherwise and we need at least one variant as otherwise the enum
+//     would be uninhabited and at least dereferencing such pointers would
+//     be UB.
+#[cfg_attr(not(doc), repr(u8))] // work around https://github.com/rust-lang/rust/issues/90435
+pub enum c_void {
+    __variant1,
+    __variant2,
+}
+pub type pid_t = i32;
+pub type uid_t = u32;
+
+pub const SA_ONSTACK: c_int = 0x08000000;
+pub const SA_NOCLDWAIT: c_int = 0x00000002;
+pub const SA_SIGINFO: c_int = 0x00000004;
+
+// Internal, for casts to access union fields
+#[repr(C)]
+pub struct sifields_sigchld {
+    si_pid: pid_t,
+    si_uid: uid_t,
+    si_status: c_int,
+    si_utime: c_long,
+    si_stime: c_long,
+}
+impl Copy for sifields_sigchld {}
+impl Clone for sifields_sigchld {
+    fn clone(&self) -> sifields_sigchld {
+        *self
+    }
+}
+
+// Internal, for casts to access union fields
+#[repr(C)]
+union sifields {
+    _align_pointer: *mut c_void,
+    sigchld: sifields_sigchld,
+}
+// Internal, for casts to access union fields. Note that some variants
+// of sifields start with a pointer, which makes the alignment of
+// sifields vary on 32-bit and 64-bit architectures.
+#[repr(C)]
+struct siginfo_f {
+    _siginfo_base: [c_int; 3],
+    sifields: sifields,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct siginfo_t {
+    pub si_signo: CInt,
+    pub si_errno: CInt,
+    pub si_code: CInt,
+    #[doc(hidden)]
+    #[deprecated(
+        since = "0.2.54",
+        note = "Please leave a comment on \
+                  https://github.com/rust-lang/libc/pull/1316 if you're using \
+                  this field"
+    )]
+    pub _pad: [CInt; 29],
+    _align: [u64; 0],
+}
+
+pub type CInt = i32;
+pub type SizeT = usize;
+pub type SighandlerT = SizeT;
+
+#[repr(C)]
+pub struct sigset_t {
+    #[cfg(target_pointer_width = "64")]
+    __val: [u64; 16],
+}
+
+#[repr(C)]
+pub struct sigaction {
+    pub sa_sigaction: SighandlerT,
+    pub sa_mask: sigset_t,
+    pub sa_flags: CInt,
+    pub sa_restorer: Option<extern "C" fn()>,
+}
+
+impl siginfo_t {
+    unsafe fn sifields(&self) -> &sifields {
+        &(*(self as *const siginfo_t as *const siginfo_f)).sifields
+    }
+
+    pub unsafe fn si_pid(&self) -> pid_t {
+        self.sifields().sigchld.si_pid
+    }
+
+    pub unsafe fn si_uid(&self) -> uid_t {
+        self.sifields().sigchld.si_uid
+    }
+
+    pub unsafe fn si_status(&self) -> c_int {
+        self.sifields().sigchld.si_status
+    }
+
+    pub unsafe fn si_utime(&self) -> c_long {
+        self.sifields().sigchld.si_utime
+    }
+
+    pub unsafe fn si_stime(&self) -> c_long {
+        self.sifields().sigchld.si_stime
+    }
+}
+
+extern "C" {
+    pub fn sigemptyset(set: *mut sigset_t) -> CInt;
+
+    pub fn sigaction(
+        signum: CInt,
+        act: *const sigaction,
+        oldact: *mut sigaction,
+    ) -> CInt;
+}
