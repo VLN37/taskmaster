ac354cc3eb69c1b6fdc20f58e06c0e8fc0ebc502
VLN37 <jfpr1237@gmail.com>
2023-11-17 09:27:14 -0300

    rework: backend to work with the polling server

--- masterlib/src/daemon/backend.rs
+++ masterlib/src/daemon/backend.rs
@@ -1,22 +1,40 @@
 use std::{
+    collections::HashMap,
     error::Error,
     io::{Read, Write},
     os::unix::net::UnixStream,
 };
 
+use super::server::Key;
+
 #[derive(Default)]
-pub struct BackEnd {}
+pub struct BackEnd {
+    pub clients: HashMap<u64, UnixStream>,
+}
 
 impl BackEnd {
     pub fn new() -> BackEnd {
         BackEnd::default()
     }
 
-    pub fn process(&self, client: &mut UnixStream) -> Result<(), Box<dyn Error>> {
+    pub fn recv(&mut self, key: Key) -> Result<(), Box<dyn Error>> {
         let mut buf = String::new();
-        client.read_to_string(&mut buf)?;
-        println!("client: |{buf}|");
-        client.write_all(b"message received")?;
+        if let Some(client) = self.clients.get_mut(&key) {
+            client.read_to_string(&mut buf)?;
+            println!("#{key} RECEIVED: |{buf}|");
+        } else {
+            println!("backend: invalid key {key}");
+        }
+        Ok(())
+    }
+
+    pub fn send(&mut self, key: Key) -> Result<(), Box<dyn Error>> {
+        if let Some(client) = self.clients.get_mut(&key) {
+            client.write_all(b"message received")?;
+            client.shutdown(std::net::Shutdown::Both)?;
+        } else {
+            println!("backend: invalid key {key}");
+        }
         Ok(())
     }
 }
