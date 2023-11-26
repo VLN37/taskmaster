adf462169dc5699c18af38baeaddf92bbc5688d1
Paulo Santana <paulo.santana.r@gmail.com>
2023-11-26 11:00:53 -0300

    refactor: remove ProgramStatus from config

--- daemon/src/config/structs.rs
+++ daemon/src/config/structs.rs
@@ -1,9 +1,7 @@
 mod program;
-mod program_status;
 mod restart_option;
 mod signals;
 
 pub use program::Program;
-pub use program_status::ProgramStatus;
 pub use restart_option::RestartOption;
 pub use signals::Signal;
