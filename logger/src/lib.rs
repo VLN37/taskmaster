f76ea210128ecea30a063ccd713b624f2e10d9af
VLN37 <jfpr1237@gmail.com>
2023-11-28 23:46:33 -0300

    feat: log levels with env variable support

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -1,30 +1,74 @@
+use std::env;
 use std::ffi::CString;
+use std::sync::OnceLock;
 
+#[derive(Debug)]
 pub struct Logger {
-    module: String,
+    pub module:    String,
+    pub log_level: LogLevel,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub enum LogLevel {
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG,
+}
+
+impl From<&str> for LogLevel {
+    fn from(value: &str) -> Self {
+        match value {
+            "DEBUG" => LogLevel::DEBUG,
+            "INFO" => LogLevel::INFO,
+            "WARN" => LogLevel::WARN,
+            "WARNING" => LogLevel::WARN,
+            "ERROR" => LogLevel::ERROR,
+            _ => panic!("invalid"),
+        }
+    }
+}
+
+impl LogLevel {
+    fn global_log_level() -> &'static LogLevel {
+        static LOG_LEVEL: OnceLock<LogLevel> = OnceLock::new();
+        match env::var("LOG_LEVEL") {
+            Ok(var) => return LOG_LEVEL.get_or_init(|| LogLevel::from(var.as_str())),
+            Err(_) => return LOG_LEVEL.get_or_init(|| LogLevel::INFO),
+        }
+    }
 }
 
 impl Logger {
     pub fn new(module: &str) -> Logger {
         Logger {
-            module: module.into(),
+            module:    module.into(),
+            log_level: LogLevel::global_log_level().clone(),
         }
     }
 
     pub fn debug(&self, msg: &str) {
-        println!("[{}][{:5}] {}: {}", current_time(), "DEBUG", self.module, msg)
+        if self.log_level >= LogLevel::DEBUG {
+            println!("[{}][{:5}] {}: {}", current_time(), "DEBUG", self.module, msg);
+        }
     }
 
     pub fn info(&self, msg: &str) {
-        println!("[{}][{:5}] {}: {}", current_time(), "INFO", self.module, msg)
+        if self.log_level >= LogLevel::INFO {
+            println!("[{}][{:5}] {}: {}", current_time(), "INFO", self.module, msg);
+        }
     }
 
     pub fn warn(&self, msg: &str) {
-        println!("[{}][{:5}] {}: {}", current_time(), "WARN", self.module, msg)
+        if self.log_level >= LogLevel::WARN {
+            println!("[{}][{:5}] {}: {}", current_time(), "WARN", self.module, msg);
+        }
     }
 
     pub fn error(&self, msg: &str) {
-        println!("[{}][{:5}] {}: {}", current_time(), "ERROR", self.module, msg)
+        if self.log_level >= LogLevel::ERROR {
+            println!("[{}][{:5}] {}: {}", current_time(), "ERROR", self.module, msg);
+        }
     }
 }
 
