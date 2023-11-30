4446d4a81365dcb3f7c6ad7a985b7f1b36e65efc
VLN37 <jfpr1237@gmail.com>
2023-11-29 23:44:58 -0300

    feat: macro version of the logger

--- logger/examples/logger.rs
+++ logger/examples/logger.rs
@@ -1,4 +1,4 @@
-use logger::Logger;
+use logger::{debug, error, info, warning, Logger};
 
 fn main() {
     let log = Logger::new("main");
@@ -7,7 +7,11 @@ fn main() {
     log.warn("logando");
     log.error("logando");
     let log = Logger::new("main");
-    println!("{:?}", log.log_level);
-
-    println!("{:?}", log);
+    // println!("{:?}", log.log_level);
+    // println!("{:?}", log);
+    debug!("{:?}", log);
+    info!("#{} AWAITING", 42);
+    // self.log.info(&format!("#{} AWAITING", self.server.key));
+    warning!("{:?}", log);
+    error!("{:?}", log);
 }
