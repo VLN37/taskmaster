0e77c018aa578f7aa8ee7987164c5d1e21dd8165
Paulo Santana <paulo.santana.r@gmail.com>
2023-12-01 18:42:25 -0300

    feat: spawn config programs at backend startup

--- daemon/src/backend.rs
+++ daemon/src/backend.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::io::{self, Error};
+use std::process::{id, Child, Command};
 
 use common::server::{Key, Request};
 use common::CONFIG_PATH;
@@ -9,8 +11,17 @@ use crate::TaskMasterConfig;
 
 #[derive(Default)]
 pub struct BackEnd {
-    pub config:  TaskMasterConfig,
-    pub clients: HashMap<Key, Request>,
+    pub config:   TaskMasterConfig,
+    pub programs: HashMap<String, ActiveProgram>,
+    pub clients:  HashMap<Key, Request>,
+}
+
+pub struct ActiveProgram {
+    pub config_name: String,
+    pub config:      Program,
+    pub command:     Command,
+    pub processes:   Vec<Result<Child, Error>>,
+    pub status:      Vec<ProgramStatus>,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -34,8 +45,90 @@ impl BackEnd {
     pub fn get_response_for(&self, key: Key) -> String { format!("Response for {key}") }
 
     pub fn start(&mut self) {
-        // self.config = read_config_file();
         print_programs("initial programs", &self.config.programs);
+        self.programs = Self::create_programs(&self.config.programs);
+
+        self.start_procesess();
+
+        self.programs.iter().for_each(|(_, program)| {
+            program
+                .processes
+                .iter()
+                .enumerate()
+                .for_each(|(i, _process)| {
+                    let pid_or_error = match _process {
+                        Ok(child) => child.id().to_string(),
+                        Err(err) => err.to_string(),
+                    };
+                    println!(
+                        "{}[{}]: {:?} [{}]",
+                        program.config_name, i, program.status[i], pid_or_error
+                    );
+                })
+        })
+    }
+
+    fn start_procesess(&mut self) {
+        self.programs.iter_mut().for_each(|(_name, program)| {
+            program.processes = (0..program.config.processes)
+                .map(|index| {
+                    if program.command.get_program() == "" {
+                        program.status[index as usize] = ProgramStatus::FailedToStart;
+                        return Result::Err(Error::new(
+                            io::ErrorKind::Other,
+                            "Empty command",
+                        ));
+                    }
+
+                    let spawn_result = program.command.spawn();
+                    program.status[index as usize] = match spawn_result {
+                        Ok(_) => ProgramStatus::Active,
+                        Err(_) => ProgramStatus::FailedToStart,
+                    };
+                    spawn_result
+                })
+                .collect();
+        });
+    }
+
+    fn create_programs(
+        program_configs: &HashMap<String, Program>,
+    ) -> HashMap<String, ActiveProgram> {
+        program_configs
+            .iter()
+            .map(|(program_name, command_config)| {
+                let mut command = Command::new(&command_config.command);
+                command
+                    .current_dir(&command_config.workdir)
+                    .args(&command_config.args)
+                    .envs(
+                        command_config
+                            .environment_variables
+                            .iter()
+                            .filter_map(|var| var.split_once('='))
+                            .map(|(var, value)| (var.to_string(), value.to_string()))
+                            .collect::<HashMap<String, String>>(),
+                    );
+
+                // let status = (0..command_config.processes)
+                //     .map(|_| ProgramStatus::Starting)
+                //     .collect();
+
+                let status =
+                    vec![ProgramStatus::Starting; command_config.processes as usize];
+
+                (
+                    program_name.to_owned(),
+                    ActiveProgram {
+                        config_name: program_name.to_owned(),
+                        config: command_config.clone(),
+                        command,
+                        processes: vec![],
+                        status,
+                    },
+                )
+            })
+            .collect()
     }
 
     pub fn update(&mut self, new_config: TaskMasterConfig) -> Result<(), ConfigError> {
