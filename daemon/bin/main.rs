4dbe7a165da44025f59d7c9144b9d871a361f2b1
VLN37 <jfpr1237@gmail.com>
2024-02-25 22:36:06 -0300

    feat: apply server socket parametrization

--- daemon/bin/main.rs
+++ daemon/bin/main.rs
@@ -4,13 +4,27 @@ use daemon::defs::DFL_CONFIG_FILE;
 use daemon::taskmaster::{Status, TaskMaster};
 use logger::info;
 
+fn get_config_file() -> Result<String, Box<dyn Error>> {
+    let mut arguments: Vec<String> = std::env::args().collect();
+    let mut config_file = DFL_CONFIG_FILE.into();
+
+    if arguments.len() > 2 {
+        return Err("invalid number of arguments".into());
+    }
+    if arguments.len() > 1 {
+        config_file = arguments.pop().unwrap();
+    }
+    Ok(config_file)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let config_file = get_config_file()?;
     let mut taskmaster = TaskMaster::new();
 
     '_config: loop {
         info!("Configuring...");
         match taskmaster.status {
-            Status::Starting => taskmaster.build(DFL_CONFIG_FILE)?,
+            Status::Starting => taskmaster.build(&config_file)?,
             Status::Reloading => taskmaster.reload()?,
             Status::Active => info!("All Good!"),
         }
