66f6a6b455dad2ae1cfb8910519af3bccab1da93
VLN37 <jfpr1237@gmail.com>
2024-05-03 22:09:24 -0300

    refactor: split cmd module

--- common/src/cmd.rs
+++ common/src/cmd.rs
@@ -1,59 +1,16 @@
-use core::fmt;
+mod class;
+mod error;
+mod error_kind;
+
 use std::result;
+
+pub use class::Cmd;
+pub use error::CmdError;
+pub use error_kind::CmdErrorKind;
 
 use crate::request::Request;
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum Cmd {
-    Log,
-    Status,
-    Head,
-    Attach,
-    Other(String),
-}
-
-impl Cmd {
-    pub fn parse(input: &str) -> Result<Cmd> {
-        match input.to_uppercase().as_str() {
-            "LOG" => Ok(Cmd::Log),
-            "STATUS" => Ok(Cmd::Status),
-            "HEAD" => Ok(Cmd::Head),
-            "ATTACH" => Ok(Cmd::Attach),
-            other => Ok(Cmd::Other(other.into())),
-        }
-    }
-}
-
-impl From<Cmd> for String {
-    fn from(value: Cmd) -> Self {
-        match &value {
-            Cmd::Log => "LOG".to_string(),
-            Cmd::Status => "STATUS".to_string(),
-            Cmd::Head => "HEAD".to_string(),
-            Cmd::Attach => "ATTACH".to_string(),
-            Cmd::Other(cmd) => cmd.to_string(),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum CmdErrorKind {
-    NotFound(String),
-    InvalidArguments,
-}
-
-impl From<CmdErrorKind> for String {
-    fn from(value: CmdErrorKind) -> Self {
-        match value {
-            CmdErrorKind::InvalidArguments => {
-                "ATTACH requires a program argument".into()
-            }
-            CmdErrorKind::NotFound(program) => {
-                format!("{program} is not a Taskmaster Program")
-            }
-        }
-    }
-}
+pub type Result<Cmd> = result::Result<Cmd, CmdError>;
 
 pub trait CmdHandler {
     fn handle(&mut self, request: &mut Request) -> result::Result<String, CmdError>;
@@ -62,69 +19,4 @@ pub trait CmdHandler {
     fn head(&self, request: &mut Request) -> result::Result<String, CmdError>;
     fn status(&self, request: &mut Request) -> result::Result<String, CmdError>;
     fn other(&self, request: &mut Request) -> result::Result<String, CmdError>;
-}
-
-impl fmt::Display for Cmd {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let cmd = match self {
-            Cmd::Log => "LOG",
-            Cmd::Status => "STATUS",
-            Cmd::Head => "HEAD",
-            Cmd::Attach => "ATTACH",
-            Cmd::Other(cmd) => cmd.as_str(),
-        };
-        write!(f, "{cmd}")
-    }
-}
-
-#[derive(Debug)]
-pub struct CmdError {
-    pub message: String,
-}
-
-impl std::error::Error for CmdError {}
-pub type Result<Cmd> = result::Result<Cmd, CmdError>;
-
-impl fmt::Display for CmdError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl From<&str> for CmdError {
-    fn from(msg: &str) -> Self {
-        CmdError {
-            message: msg.into(),
-        }
-    }
-}
-
-impl From<CmdError> for String {
-    fn from(value: CmdError) -> Self { value.message }
-}
-
-impl From<CmdErrorKind> for CmdError {
-    fn from(value: CmdErrorKind) -> Self {
-        CmdError {
-            message: value.into(),
-        }
-    }
-}
-
-impl fmt::Display for CmdErrorKind {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", String::from(self.clone()))
-    }
-}
-
-impl From<String> for CmdError {
-    fn from(msg: String) -> Self { CmdError { message: msg } }
-}
-
-impl CmdError {
-    pub fn new(msg: &str) -> CmdError {
-        CmdError {
-            message: msg.into(),
-        }
-    }
 }
