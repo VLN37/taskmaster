326ec674adc549d01ee299c39da8bc6ff4509c74
VLN37 <jfpr1237@gmail.com>
2024-05-27 04:34:56 -0300

    fix: clippy updates

--- common/src/macros/syscall.rs
+++ common/src/macros/syscall.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! syscall {
     ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
+        #[allow(clippy::macro_metavars_in_unsafe)]
         let res = unsafe { libc::$fn($($arg, )*) };
         if res == -1 {
             Err(std::io::Error::last_os_error())
